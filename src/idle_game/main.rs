958243974b3b1a9aa5149500891e4a7abc148058
kevin mendoza <kevinnnnmarcelo@gmail.com>
2022-05-28 13:51:01 -0300

    Refactor: solo tenemos madera

--- src/idle_game/main.rs
+++ src/idle_game/main.rs
@@ -8,57 +8,58 @@ fn main() {
 
     const INITIAL_GOLD: f64 = 100.0;
     const INITIAL_WOOD: u32 = 0;
-    const PRICE_WOOD:f64= 10.0;
-    //const WOOD_KEY:String = String::from("wood");;
-    //let resources = vec![WOOD_KEY];
-    //let mut resources_prices = HashMap::new();
-    //prices.insert(WOOD_KEY, PRICE_WOOD);
-    let gold = Arc::new(RwLock::new(INITIAL_GOLD));
-    let wood = Arc::new(RwLock::new(INITIAL_WOOD));
-    let gold_clone = gold.clone();
-    let thread_generate_gold = thread::spawn(move || loop {
-        //println!("Start Generate");
-        thread::sleep(Duration::from_secs(5));
-        if let Ok(mut _gold) = gold_clone.write() {
-            let percentage :f64 = rand::thread_rng().gen();
-            let gold_old = *_gold;
-            *_gold = gold_old + (percentage * gold_old);
-            //println!("Generate Gold: {} ",*_gold);
-        }
-        //println!("Finish Generate");
-        //println!();
-    });
-    let gold_clone2 = gold.clone();
-    let wood_clone = wood.clone();
-    let thread_informate = thread::spawn(move||loop {
+    const PRICE_WOOD:f64 = 50.0;
+    const SLEEP_TIME:u64 = 3;
+
+    let gold_lock = Arc::new(RwLock::new(INITIAL_GOLD));
+    let wood_lock = Arc::new(RwLock::new(INITIAL_WOOD));
+
+    let gold_generate = gold_lock.clone();
+    let gold_info = gold_lock.clone();
+    let gold_exchange = gold_lock.clone();
+    let wood_info = wood_lock.clone();
+    let wood_exchange = wood_lock.clone();
+
+    let thread_handle_info = thread::spawn(move||loop {
         println!("==========Information=========");
-        thread::sleep(Duration::from_secs(1));
-        if let Ok(mut _gold) = gold_clone2.read() {
+        if let Ok(mut _gold) = gold_info.read() {
             println!("Gold: {} ",*_gold);
         }
-        if let Ok(mut _wood) = wood_clone.read() {
+        if let Ok(mut _wood) = wood_info.read() {
             println!("Wood: {} ",*_wood);
         }
         println!("==============================");
         println!();
+        thread::sleep(Duration::from_secs(SLEEP_TIME));
     });
-    let gold_clone3 = gold.clone();
-    let wood_clone2 = wood.clone();
-    let thread_exchange = thread::spawn(move||loop {
-        //println!("Start Exchange");
-        thread::sleep(Duration::from_secs(1));
-        if let Ok(mut _gold) = gold_clone3.write() {
-            if let Ok(mut _wood) = wood_clone2.write() {
+
+    let thread_handle_generate_gold = thread::spawn(move || loop {
+        println!("Start Generate Gold");
+        if let Ok(mut _gold) = gold_generate.write() {
+            let percentage :f64 = rand::thread_rng().gen();
+            *_gold = *_gold + (percentage * (*_gold));
+        }
+        println!("Finish Generate Gold");
+        println!();
+        thread::sleep(Duration::from_secs(SLEEP_TIME));
+    });
+
+    let thread_handle_exchange = thread::spawn(move||loop {
+        println!("Start Exchange");
+        if let Ok(mut _gold) = gold_exchange.write() {
+            if let Ok(mut _wood) = wood_exchange.write() {
                 if *_gold > PRICE_WOOD {
                     *_gold = *_gold - PRICE_WOOD;
                     *_wood = *_wood + 1;
                 }
             }
         }
-        //println!("Finish Exchange");
-        //println!();
+        println!("Finish Exchange");
+        println!();
+        thread::sleep(Duration::from_secs(SLEEP_TIME));
     });
-    thread_generate_gold.join().unwrap();
-    thread_informate.join().unwrap();
-    thread_exchange.join().unwrap();
+
+    thread_handle_generate_gold.join().unwrap();
+    thread_handle_info.join().unwrap();
+    thread_handle_exchange.join().unwrap();
 }
